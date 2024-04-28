bb2485c496d9258efd04f5d02b5820a45b43c120
First Name Last Name <72994203+First-Name-Last-Name@users.noreply.github.com>
2024-04-28 15:41:55 +0200

    Add files via upload

--- src/main/java/com/example/mixin/MangroveRootsBlockMixin.java
+++ src/main/java/com/example/mixin/MangroveRootsBlockMixin.java
@@ -16,6 +16,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
